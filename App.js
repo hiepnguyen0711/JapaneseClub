e04d23a09218e8b3b3ffb8bd73fafde9818d6a42
hiepnguyen0711 <nguyenvanhiep0711@gmail.com>
2023-07-21 18:37:07 +0700

    test

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  const baseUrl = 'http://127.0.0.1/api_tintuc.php?id=2';
+  const [data, setData] = useState([]);
+  useEffect(async () => {
+   
+    await axios.patch(baseUrl, {
+      vatpham: '4 chai nước suối'
+    }).then((res) => console.log(res))
+    .catch((e) => console.log(e));
+
+    await axios({
+      method: 'get',
+      url: baseUrl
+    }).then((res) => {
+      setData(res.data);
+    }).catch((e) => {
+      console.log(e);
+    })
+    
+    
+    
+  }, []);
+  const luckyBoxItem = (itemData) => {
+    return(
+      <View>
+        <Text>{itemData.item.id}</Text>
+        <Text>{itemData.item.vatpham}</Text>
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+          <FlatList 
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={luckyBoxItem}
+          />
+      </View>
+    </>
   );
 }
 
